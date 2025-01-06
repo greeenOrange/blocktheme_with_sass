5210123bf33c05d1dd691ff416829b64e5cdf7ad
greeenOrange <gm1sadiq@gmail.com>
2025-01-06 11:27:03 +0600

    first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '5451bf579b1ab3251cda');
---
+++ src/Components/Screens/Home/Home.js
@@ -0,0 +1,15 @@
+import Header from '../../Header'
+import Banner from '../../Partials/Banner'
+
+const Home = () => {
+  return (
+     <>
+        <Header />
+     <main>
+      <Banner />
+      </main>
+     </>
+  )
+}
+
+export default Home
